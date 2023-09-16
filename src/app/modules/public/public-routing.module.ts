246b3184c888f79c4e459a2b7d4881a494c33e33
evandrogouveia <vando_gouveia@hotmail.com>
2023-09-15 22:42:44 -0300

    conteudos

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -4,7 +4,11 @@ import { PublicComponent } from './public.component';
 
 const routes: Routes = [
   {
-    path: '', component: PublicComponent, children: []
+    path: '', component: PublicComponent, children: [
+      { 
+        path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) 
+      },
+    ]
   }
 ];
 
